bea7754409193c1da4fb0a7a29a3227fceefb453
jparisu <javierparis@eprosima.com>
2021-03-05 13:12:30 +0100

    Refs #10655: fix FastDDSS in include
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- performance_test/src/experiment_configuration/experiment_configuration.cpp
+++ performance_test/src/experiment_configuration/experiment_configuration.cpp
@@ -214,7 +214,7 @@ void ExperimentConfiguration::setup(int argc, char ** argv)
               "You must compile with FastRTPS support to enable it as communication mean.");
 #endif
     } else if (vm["communication"].as<std::string>() == "FastDDS") {
-#ifdef PERFORMANCE_TEST_FASTDDSS_ENABLED
+#ifdef PERFORMANCE_TEST_FASTDDS_ENABLED
       m_com_mean = CommunicationMean::FASTDDS;
       #ifdef PERFORMANCE_TEST_ODB_FOR_SQL_ENABLED
       m_com_mean_str = "FASTDDS";
